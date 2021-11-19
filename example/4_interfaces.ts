27441216f717c7bf61b5843b6a65bec007313d91
jinho <gogod23@ajou.ac.kr>
2021-11-19 16:48:50 +0900

    study #2

--- example/4_interfaces.ts
+++ example/4_interfaces.ts
@@ -4,8 +4,8 @@ interface User {
   age: number;
 }
 
-// 변수에 사용하는 경우
-const seho: User = { name: 'hi', age: 100 };
+// 0. 변수에 사용하는 경우
+const seho: User = { name: "hi", age: 100 };
 
 // 함수의 매개변수에 사용하는 경우
 function getUser(user: User) {
@@ -13,29 +13,99 @@ function getUser(user: User) {
 }
 getUser(seho);
 
-// 함수의 전체 타입에 사용하는 경우
-// interface SumFunction {
-//   (a: number, b: number): number;
-// }
-// let sum: SumFunction;
-// sum = function (num1: number, num2: string): number {
-//   return num1 + num2;
-// };
-
-// 배열의 인덱싱에 사용하는 경우
-// interface StringArray {
-//   [index: number]: string;
-// }
-// let arr: StringArray;
-// arr[0] = 'hi';
-// arr[1] = 10;
-
 // 인터페이스 확장
 interface Person {
   name: string;
-  age: number; // 옵셔널 선택자 ? 동일하게 적용 가능
+  age: number; // 옵셔널 선택자 ?. 동일하게 적용 가능
 }
 interface Developer extends Person {
   language: string;
 }
-const joo: Developer = { name: 'joo', age: 20, language: 'ts' };
+const joo: Developer = { name: "joo", age: 20, language: "ts" };
+
+///////
+
+interface User {
+  name: string;
+  age: number;
+}
+
+const u1: User = {
+  name: " abc",
+  age: 30,
+};
+
+// 1. 가장 많이 활용되는 예 -> 함수의 인자에 인터페이스 활용
+function addUser(user: User): void {
+  console.log(user);
+}
+
+interface typedef {
+  name: string;
+  age: number;
+  sex: boolean;
+}
+
+const deleteUser = (index: number, st?: string, ...args: number[]): void => {
+  console.log("foo");
+};
+
+// 2. 함수 인터페이스
+// 스니펫 형태로 만들어 놓고 협업 하기에 유용
+
+interface func {
+  (a: number, b: number): number;
+}
+
+var func1: func;
+
+// 1
+func1 = function (a, b) {
+  return 1;
+};
+
+// 2
+const func2: func = () => {
+  return 1;
+};
+
+// 3. 인덱싱(배열에 적용)
+
+interface stringArray {
+  [index: number]: string;
+}
+
+let strArr: stringArray = ["a", "b", "c"];
+let strArr2: string[] = ["asdf", "asdf", "asdf"];
+
+// 3. 인터페이스 딕셔너리 패턴 ***
+
+interface StringRegexDic {
+  // 객체에 적용 가능한듯
+  // 일반 객체 인터페이스완 다르게 key값 또한 타이핑 적용 가능
+  [key: string]: RegExp; // key : value
+}
+
+var obj2: StringRegexDic = {
+  sth: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+};
+
+// 4. 인터페이스 확장
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Student extends Person {
+  studentNum?: number;
+}7
+
+let s1: Student = {
+  name: "amy",
+  age: 30,
+};
+
+
--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,9 +1,20 @@
-let todoItems: any[];
+type todo = {
+  // type  alias
+  id: number;
+};
+
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
 //{ id: number; title: string; done: boolean }
 
 // api
-function fetchTodoItems(): object[] {
-  const todos: object[] = [
+function fetchTodoItems(): Todo[] {
+  const todos: Todo[] = [
     { id: 1, title: "안녕", done: false },
     { id: 2, title: "타입", done: false },
     { id: 3, title: "스크립트", done: false },
@@ -12,12 +23,12 @@ function fetchTodoItems(): object[] {
 }
 
 // crud methods
-function fetchTodos(): object[] {
+function fetchTodos(): Todo[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo: object[]): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -25,7 +36,7 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: object[]): void {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -42,6 +53,8 @@ function showCompleted(): object[] {
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
 function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  addTodo({ id: 4, title: "ff", done: false });
+  addTodo({ id: 5, title: "fff", done: false });
 }
 
 // NOTE: 유틸 함수
